ffdaa29796e59a4fbeaafb009a005c762483ff1c
cy322666 <cy322666@gmail.com>
2022-11-25 13:50:01 +0200

    first comm

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -12,7 +13,7 @@ class EventController extends Controller
         $data = $request->toArray()['leads']['status'][0];
 
         Event::query()->create([
-            'change_at'   => '',
+            'change_at'   => Carbon::now('Europe/Moscow')->format('Y-m-d H:i:s'),
             'lead_id'     => $data['id'],
             'status_at'   => $data['old_status_id'],
             'status_to'   => $data['status_id'],
